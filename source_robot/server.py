bbd4c8af0939fbee963e7db633940fed37221f27
daan <daniel@bitofpepper.com>
2019-12-12 14:27:12 +0100

    tpo :(((

--- source_robot/server.py
+++ source_robot/server.py
@@ -32,7 +32,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 
-q = Queue
+q = Queue()
 
 def main_robot_control(q_out):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
