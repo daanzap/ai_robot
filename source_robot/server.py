0a9a69dd5a0ab07b2dda156283be6a9f9713a65b
daan <daniel@bitofpepper.com>
2019-12-12 15:00:37 +0100

    starting video stream

--- source_robot/server.py
+++ source_robot/server.py
@@ -48,7 +48,7 @@ def main_robot_control(q_out):
         message = c.recv(1024)
         # message = 'Got connection from {}'.format(addr)
         if message == b'connection request':
-            subprocess.Popen('raspivid -v -w 640 -h 480 -fps 30 -n -t 0 -l -o tcp://0.0.0.0:5001')
+            proc_id = subprocess.Popen(["raspivid","-v","-w","640", "-h", "480", "-fps","30","-n","-t", "0", "-l", "-o", "tcp://0.0.0.0:5001"])
             c.send('connection ok'.encode('utf-8'))
 
         q_out.put(message)
