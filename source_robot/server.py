795276986d8288f303abc7591a71403f1e133f0a
daan <daniel@bitofpepper.com>
2019-12-12 14:11:05 +0100

    seperate server socket server

--- source_robot/server.py
+++ source_robot/server.py
@@ -2,6 +2,8 @@
 import json
 import socket
 import time
+from queue import Queue
+from threading import Thread
 
 robot_name = 'robat'
 
@@ -29,23 +31,34 @@ dest = ('<broadcast>', 10100)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 9999))
-# become a server socket
-serversocket.listen(5)
 
+q = Queue
+
+def main_robot_control(q_out):
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to a public host, and a well-known port
+    serversocket.bind((socket.gethostname(), 9999))
+    # become a server socket
+    serversocket.listen(5)
+    while True:
+        c, addr = serversocket.accept()
+        print('Got connection from {}'.format(addr))
+        c.send('Connecting with {}'.format(robot_name))
+        message = c.recv(1024)
+        q.put(message)
+
+
+main_server = Thread(target=main_robot_control, args=(q,), daemon=True)
+main_server.start()
 data = 'no data yet'
 while True:
     print(msg)
     s.setblocking(0)
     s.sendto(msg, dest)
     try:
-        c, addr = serversocket.accept()
-        print('Got connection from {}'.format(addr))
-        c.send('Connecting with {}'.format(robot_name))
-        c.recv(1024)
+        message = q.get_nowait()
+        data = message
     except Exception as e:
-        print(e)
+        pass
     print(data)
 
