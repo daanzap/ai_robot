1da73a26abd04971628753071be4270ef51a674b
daan <daniel@bitofpepper.com>
2020-05-13 13:24:49 +0200

    fixed import again again

--- source_robot/server.py
+++ source_robot/server.py
@@ -7,9 +7,9 @@ from threading import Thread
 import subprocess
 
 
-from .robot_motor_control import MotorControl
+from source_robot.robot_motor_control import MotorControl
 
-from .settings_robot import robot_name
+from source_robot.settings_robot import robot_name
 
 import logging
 
