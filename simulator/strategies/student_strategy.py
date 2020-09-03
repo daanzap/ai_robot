e4ba6680b4ca9bfe2ed9b8668dccbb85d986dfb2
daan <daniel@bitofpepper.com>
2020-09-03 11:54:04 +0200

    movel models to seperate dir

--- simulator/strategies/student_strategy.py
+++ simulator/strategies/student_strategy.py
@@ -1,15 +1,40 @@
 import numpy as np
+from tensorflow.python.keras.models import load_model
+
 from .auto_pilot import AutoSteer
 from joblib import load
 import os
 
 class AutoSteerStudent(AutoSteer):
-    #pass
+    pass
 
-    ######## skLearn implementation#################
+    # ######## skLearn implementation#################
+    # def __init__(self):
+    #     self.clf = load(os.path.join('..', 'models', 'auto_pliot.joblib'))
+    #
+    #
+    # def steer(self,view):
+    #     reduce_camera_image = 5
+    #     width = view.shape[1]
+    #     height = view.shape[0]
+    #     dim = (int(width / reduce_camera_image), int(height / reduce_camera_image))
+    #     camera_view_reduced = view[::reduce_camera_image, ::reduce_camera_image]
+    #     size_array = dim[0] * dim[1] * 3  # width height time 3 channels
+    #     image_data = camera_view_reduced.reshape(1, size_array)
+    #     prediction = self.clf.predict(image_data)
+    #     print(prediction)
+    #     if prediction[0] == 0:
+    #         return 'forward'
+    #     elif prediction[0] == 1:
+    #         return 'left'
+    #     elif prediction[0] == 2:
+    #         return 'right'
+    #     else:
+    #         return 'forward'
+
+    ################## Keras deeplearning implementation ###################################
     def __init__(self):
-        self.clf = load(os.path.join('..', 'notebooks', 'auto_pliot.joblib'))
-
+        self.model = load_model(os.path.join('..', 'models', 'my-keras-model.h5'))
 
     def steer(self,view):
         reduce_camera_image = 5
@@ -17,17 +42,14 @@ class AutoSteerStudent(AutoSteer):
         height = view.shape[0]
         dim = (int(width / reduce_camera_image), int(height / reduce_camera_image))
         camera_view_reduced = view[::reduce_camera_image, ::reduce_camera_image]
-        size_array = dim[0] * dim[1] * 3  # width height time 3 channels
-        image_data = camera_view_reduced.reshape(1, size_array)
-        prediction = self.clf.predict(image_data)
+        camera_view_reduced = np.array([camera_view_reduced.astype('float32') / 255 ])
+        prediction = self.model.predict_classes(camera_view_reduced)[0]
         print(prediction)
-        if prediction[0] == 0:
+        if prediction == 0:
             return 'forward'
-        elif prediction[0] == 1:
+        elif prediction == 1:
             return 'left'
-        elif prediction[0] == 2:
+        elif prediction == 2:
             return 'right'
         else:
             return 'forward'
-
-    ################## Keras deeplearning implementation ###################################
